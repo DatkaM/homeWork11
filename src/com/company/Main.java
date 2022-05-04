f0884f4f9bd479db47f84ee70ef1ef70305018fa
zhusupmamatzhanov <32612854+zhusupmamatzhanov@users.noreply.github.com>
2022-05-04 19:51:09 +0600

    homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.area(10,20,10);
+
+
+
+
+    }
+}
